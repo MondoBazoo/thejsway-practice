653ee57357ff4ce761592328d57aa495ee012afb
MondoBazoo <isaiahrivera850@gmail.com>
2022-06-22 17:29:33 -0700

    add new thejsway-practice

---
+++ chapter07-md/sum-of-values/main.js
@@ -0,0 +1,9 @@
+//Write a program that creates the following array, then calculates and shows the sum of its values (42 in that case).
+const values = [3, 11, 7, 2,9, 10];
+let sum = 0;
+
+for (let i = 0; i < values.length; i++) {
+    sum += values[i];
+}
+console.log(sum);
+
